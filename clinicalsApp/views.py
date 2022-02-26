d811186e5aae2fd83b195978b75b4ccc0f297037
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-02-26 17:49:50 +0530

    implement add clinical data view

--- clinicalsApp/views.py
+++ clinicalsApp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from clinicalsApp.models import Patient
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from clinicalsApp.forms import ClinicalDataForm
 
 
 # Create your views here.
@@ -24,3 +25,14 @@ class PatientUpdateView(UpdateView):
 class PatientDeleteView(DeleteView):
     model = Patient
     success_url = reverse_lazy('index')
+
+
+def addData(request, **kwargs):
+    form = ClinicalDataForm()
+    patient = Patient.objects.get(id=kwargs['pk'])
+    if request.method == 'POST':
+        form = ClinicalDataForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    return render(request, 'clinicalsApp/clinicaldata_form.html', {"form": form, 'patient': patient})

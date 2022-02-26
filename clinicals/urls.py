776def0edafca49e10e0f2dd723a7b812a1ffff4
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-02-26 17:22:03 +0530

    implementing & testing delete patient

--- clinicals/urls.py
+++ clinicals/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.PatientListView.as_view(), name='index'),
     path('create/', views.PatientCreateView.as_view()),
     path('update/<int:pk>/', views.PatientUpdateView.as_view()),
+    path('delete/<int:pk>/', views.PatientDeleteView.as_view()),
 ]
